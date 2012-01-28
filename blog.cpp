33cf4988dd6a8398f803894f5ef4b032eb0424d7
Misha Ketslah <thezogg@gmil.com>
2012-01-28 15:47:49 +0200

    for test only using defined blog

--- blog.cpp
+++ blog.cpp
@@ -28,23 +28,19 @@ void Blog::parser(QNetworkReply *reply)      //parsing data due to request
 
 void Blog::blog_request(QString blog) //gallery, for now period is not used, maybe in future we'll use it for time period
 {
-    QString key="your_api_key";
-    QUrl url("http://api.tumblr.com/v2/blog/"+blog+".tumblr.com/info?api_key="+key);
+    QString key="api_key";
+   // QUrl url("http://api.tumblr.com/v2/blog/"+blog+".tumblr.com/info?api_key="+key);
 
-    QByteArray postdata;
-
-    //POST data
-
-       postdata.append(QString("key=").toUtf8());
-       postdata.append(QUrl::toPercentEncoding(key));
-
-   //END POST data
-
+    QUrl url("http://api.tumblr.com/v2/blog/russialize.com/posts/text?api_key="+key); //just for begining i would use only my blog
 
      QNetworkAccessManager *networkManager = new QNetworkAccessManager(this);
 
      connect(networkManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(parser(QNetworkReply*)));
      networkManager->get(QNetworkRequest(url));
+     //QUrl url("http:/api.tumblr.com/v2/blog/david.tumblr.com/avatar"); // for avatar
+
+
+
 
 }
 
